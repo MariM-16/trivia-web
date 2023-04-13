0235e97f096c6f8ca61f99e51f599194617fb339
Antonia Carrizo <aacarrizo@miuandes.cl>
2023-04-13 09:28:48 -0400

    recibir respuestas

--- app.js
+++ app.js
@@ -9,6 +9,15 @@ let respuestaEnviada;
 let tiempoPreguntaSelect;
 let tiempoRespuestaSelect;
 let nombreInput;
+let span = document.getElementsByClassName("close")[0];
+let respuestaE = 0;
+let respuestaR = 0;
+let cambiojugador = 1;
+let descalificaciones = 0;
+let rondas = 5;
+let contadorRondas = 0;
+
+let respuestas = ["chelo", "piano", "violin", "guitarra", "tambor"]; // Puntajes de los jugadores
 
 if (pathname.includes("create_game.html")) {
 loginForm.addEventListener('submit', async (event) => {
@@ -65,6 +74,8 @@ document.getElementById('login-button').addEventListener('click', function() {
 
 
 if (pathname.includes("join_game.html")) {
+
+
     generatorGames();
     let btnCrearPartida = document.getElementById('crear');
     let modalCrearPartida = document.getElementById('modalCrearPartida');
@@ -110,7 +121,7 @@ if (pathname.includes("join_game.html")) {
       //incluimos los juegos en el contenedor
       container.appendChild(game);
 
-      console.log("Evento de clic agregado");
+
       event.preventDefault(); // Evitar que la página se recargue
       modalCrearPartida.classList.remove("displayblock");
       let botones = document.querySelectorAll(".bton");
@@ -119,6 +130,13 @@ if (pathname.includes("join_game.html")) {
           window.location.href = "start_game.html";
         });
       });
+      let divHTML = game.innerHTML;
+      localStorage.setItem("miDivGuardado", divHTML); //guardamos la sala creada en LocalStorage
+      localStorage.setItem("tiempoPreguntaSelect", tiempoPreguntaSelect); //guardamos la sala creada en LocalStorage
+      localStorage.setItem("tiempoRespuestaSelect", tiempoRespuestaSelect); //guardamos la sala creada en LocalStorage
+
+      console.log(tiempoPreguntaSelect);
+      console.log(tiempoRespuestaSelect);
     });
 
     let botones = document.querySelectorAll(".bton");
@@ -157,18 +175,47 @@ function generatorGames(){;
         container.appendChild(game);
       
     };
+    // Obtener la cadena del div guardado en localStorage
+    let divGuardado = localStorage.getItem("miDivGuardado");
+
+    // Verificar si se encontró un div guardado
+    if (divGuardado !== null) {
+      // Crear un elemento div para mostrar el div guardado
+      let divMostrado = document.createElement("div");
+      divMostrado.innerHTML = divGuardado;
+      console.log();
+      divMostrado.classList = "game";
+      // Agregar el div mostrado al DOM
+      container.appendChild(divMostrado);
+    }
+
 
 }
 
 if (pathname.includes("start_game.html")) {
+  document.getElementById('ronda').textContent = contadorRondas + "/" +rondas ;
+  document.getElementById('btn-enviar-pregunta').disabled = true;
+  let tiempoP = localStorage.getItem("tiempoPreguntaSelect");
+  let tiempoA = localStorage.getItem("tiempoRespuestaSelect");
+  let modal = document.getElementById("myModal");
+
+  if (tiempoP !== null && tiempoA !== null) {
+    tiempoPreguntaSelect = tiempoP;
+    tiempoRespuestaSelect= tiempoA;
+  }
+  console.log(tiempoPreguntaSelect);
+  console.log(tiempoRespuestaSelect);
   let currentPlayer = 1; // Jugador actual
-  let timer = tiempoPreguntaSelect; // Tiempo restante
-  let question = "¿Cuál es la capital de Francia?"; // Pregunta actual
+  let timerPregunta = tiempoPreguntaSelect; // Tiempo restante pra hacer la pregunta
+  let timerRespuesta = tiempoRespuestaSelect; // Tiempo restante para hacer la respuesta
+
+
   let playerScores = [0, 0, 0, 0, 0]; // Puntajes de los jugadores
   let strikes = [0, 0, 0, 0, 0]; // Faltas de los jugadores
   let disqualifications = [0, 0, 0, 0, 0]; // Descalificaciones de los jugadores
 
   // Función para responder a una pregunta
+  /*
   function answerQuestion(option) {
     // Verificar si la opción seleccionada es la respuesta correcta (por ejemplo, opción 1 es la respuesta correcta)
     if (option === '1') {
@@ -200,26 +247,98 @@ if (pathname.includes("start_game.html")) {
       updateTimer();
     }
   }
+*/
 
   // Función para actualizar el temporizador en la vista
-  function updateTimer() {
-    document.getElementById('timer').textContent = timer + ' segundos';
-    if (timer > 0) {
-      setTimeout(function() {
-        timer--;
-        updateTimer();
-      }, 1000);
+  let timerId;
+  function updateTimer(tipoTimer) {
+    document.getElementById('timer').textContent = tipoTimer + ' s';
+      if (tipoTimer > 0) {
+        // Limpiar el timeout anterior si existe
+        if (timerId) {
+          clearTimeout(timerId);
+        }
+        // Iniciar un nuevo timeout
+        timerId = setTimeout(function() {
+          tipoTimer--;
+          if (respuestaE==1){
+            timerRespuesta = tipoTimer;
+          }
+          updateTimer(tipoTimer);
+        }, 1000);
+      }
+    
+    if(tipoTimer==0 && respuestaE==0 ){
+      //abrir un modal
+      showFalta();
+      let currentP = "strikes-"+cambiojugador;
+      let currentS = "state-"+cambiojugador;
+
+      let faltas1=document.getElementById(currentP);
+      if(faltas1==3){
+        descalificaciones++;
+        let disq = "disqualifications-"+cambiojugador;
+        document.getElementById(disq).textContent = "SI";
+        document.getElementById(descalificaciones-totales).textContent = descalificaciones;
+      }
+      else{
+        let faltas2= parseInt(faltas1.textContent) + 1;
+
+        document.getElementById(currentP).textContent = faltas2;
+        document.getElementById(currentS).textContent = "Jugador";
+      }
+
+      cambiojugador++;
+      let currentSiguiente = "state-"+cambiojugador;
+      document.getElementById(currentSiguiente).textContent = "Pregunton";
+      document.getElementById('current-player').textContent = "jugador"+ cambiojugador+1;
+    
+
+    }
+
+    if(tipoTimer==0 && respuestaR==0 ){
+      //abrir un modal
+      showFalta();
+      let currentP = "strikes-"+cambiojugador;
+   
+
+      let faltas1=document.getElementById(currentP);
+      if(faltas1==3){
+        descalificaciones++;
+        let disq = "disqualifications-"+cambiojugador;
+        document.getElementById(disq).textContent = "SI";
+        document.getElementById(descalificaciones-totales).textContent = descalificaciones;
+      }
+      else{
+        let faltas2= parseInt(faltas1.textContent) + 1;
+
+        document.getElementById(currentP).textContent = faltas2;
+      }
     }
   }
-
-  // Iniciar el temporizador cuando se cargue la página
-  window.onload = function() {
-    updateTimer();
+  // Reiniciar el timer con un nuevo valor
+  function reiniciarTimer(nuevoValor) {
+    // Limpiar el timeout anterior si existe
+    if (timerId) {
+      clearTimeout(timerId);
+    }
+    // Iniciar un nuevo timeout con el nuevo valor
+    updateTimer(nuevoValor);
   }
+
+  // Iniciar el temporizador cuando se haga click en empezar
+  document.getElementById('btn-empezar').addEventListener('click', function() {
+    updateTimer(timerPregunta);
+    document.getElementById('btn-empezar').disabled = true;
+    document.getElementById('btn-enviar-pregunta').disabled = false;
+    contadorRondas++;
+    document.getElementById('ronda').textContent = contadorRondas + "/" +rondas ;
+  });
 
   let answerContainer = document.getElementById('answer-area');
   //vemos la pregunta que se envio 
   document.getElementById('btn-enviar-pregunta').addEventListener('click', function() {
+    respuestaE++;
     let questionInput = document.getElementById("question"); // Obtén el elemento input por su ID
     let questionValue = questionInput.value; // Accede al valor del input
     preguntaEnviada = questionValue;
@@ -238,10 +357,12 @@ if (pathname.includes("start_game.html")) {
     answerContainer.classList.remove("btn-hidden");
     answerContainer.classList.add("displayblock");
     console.log(answerContainer);
-    
+    reiniciarTimer(timerRespuesta);
   });
 
+  let respuestasContainer = document.getElementById('respuestas-area');
   document.getElementById('btn-enviar-respuesta').addEventListener('click', function() {
+    
     let answerInput = document.getElementById("answer-input"); // Obtén el elemento input por su ID
     let answerValue = answerInput.value; // Accede al valor del input
     respuestaEnviada = answerValue;
@@ -253,6 +374,53 @@ if (pathname.includes("start_game.html")) {
     textAnswer.classList.add("text-game-section");
     textAnswer.textContent = respuestaEnviada;
     answerContainer.appendChild(textAnswer);
-    
+    respuestaR++;
+    respuestasContainer.classList.remove("btn-hidden");
+    respuestasContainer.classList.add("displayblock");
+    reiniciarTimer(timerRespuesta+90);
+    setTimeout(showRespuestas(), 10000);
   });
+  function showFalta(){
+    modal.classList.toggle("modalBlock");
+    span.onclick = function() {
+        modal.classList.toggle("modalBlock");
+        if(respuestaE==0){
+          timerPregunta=tiempoPreguntaSelect;
+          updateTimer(timerPregunta);
+        }
+    }
+    
+}
+function showRespuestas(){
+  let containerRespuestas = document.getElementById("containerRespuestas");
+  for(let i = 0; i<5; i++){
+    let respuesta = document.createElement("div");
+    let selector = document.createElement('select');
+    const opciones = [
+      { valor: 'buena', texto: 'Buena' },
+      { valor: 'mas_o_menos', texto: 'Mas o menos' },
+      { valor: 'mala', texto: 'Mala' }
+    ];
+    
+    // Llenar el selector con las opciones
+    opciones.forEach(opcion => {
+      const optionElement = document.createElement('option');
+      optionElement.value = opcion.valor;
+      optionElement.textContent = opcion.texto;
+      selector.appendChild(optionElement);
+    });
+    console.log(selector);
+    
+    respuesta.classList = "respuestas";
+    respuesta.textContent = respuestas[i];
+    respuesta.appendChild(selector);
+    //incluimos las respuestas en el contenedor
+    containerRespuestas.appendChild(respuesta);
+
+    }
+}
+document.getElementById('btn-enviar-calificaciones').addEventListener('click', function() {
+  document.getElementById('btn-enviar-calificaciones').classList.toggle("btn-hidden");
+  reiniciarTimer(30);
+});
 }
